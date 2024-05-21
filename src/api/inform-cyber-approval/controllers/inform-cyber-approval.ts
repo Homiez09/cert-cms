472fd9718498d618b2b20c8dcdb85a5e5ff9dad0
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-05-21 20:07:39 +0700

    update approval

---
+++ src/api/inform-cyber-approval/controllers/inform-cyber-approval.ts
@@ -0,0 +1,7 @@
+/**
+ * inform-cyber-approval controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::inform-cyber-approval.inform-cyber-approval');
